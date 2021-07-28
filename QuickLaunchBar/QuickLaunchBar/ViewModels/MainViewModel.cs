36183bff2eca5fe6a5b99f01dc868c3127992681
King-GitHub-1998 <1762299402@qq.com>
2021-07-28 15:48:45 +0800

    test

---
+++ QuickLaunchBar/QuickLaunchBar/ViewModels/MainViewModel.cs
@@ -0,0 +1,14 @@
+﻿using QuickLaunchBar.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickLaunchBar.ViewModels
+{
+    public class MainViewModel:ViewModelBase
+    {
+
+    }
+}
