6c5748043a9a7f55e1caaabc19d462b5f98326ee
King-GitHub-1998 <1762299402@qq.com>
2021-07-28 18:19:36 +0800

    frist commit

---
+++ QuickLaunchBar/QuickLaunchBar/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QuickLaunchBar
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        #region 静态属性
+        private Rect desktopWorkingArea;
+        private System.Windows.Point anchorPoint;
+        #endregion
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            desktopWorkingArea = System.Windows.SystemParameters.WorkArea;
+            this.Width = desktopWorkingArea.Width / 1.5;
+            this.Left = desktopWorkingArea.Width/2 - (this.Width/2);
+            this.Top = desktopWorkingArea.Height  - (this.Height / 2);
+        }
+    }
+}
